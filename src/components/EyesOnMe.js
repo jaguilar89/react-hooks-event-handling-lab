0f75e4205c0b649a91f5c0c53147733c1be44852
Jose Aguilar <jaguilarny@gmail.com>
2022-06-27 19:11:32 -0400

    commit tree 318e8d5487728332c3e23ce5d68fd52b95ba049b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    return <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+}
+
+const focus = () => console.log('Good!');
+const blur = () => console.log('Hey! Eyes on me!');
+
+export default EyesOnMe;
